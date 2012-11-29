2d37df1bc82fcc8ebe46a75cb5d20d90ec344baa
Tova Linder <tovalinders@gmail.com>
2012-11-29 09:13:25 +0100

    ss

--- src/Game.java
+++ src/Game.java
@@ -6,33 +6,38 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Game{
+public class Game {
 	Board board;
 	private int winWidth, winHeight;
 
-	public Game(int cols, int rows, int winWidth, int winHeight) {
+	public Game(int winWidth, int winHeight, int cols, int rows) {
 		this.winWidth = winWidth;
 		this.winHeight = winHeight;
-		
+
 		board = new Board(rows, cols);
 		JFrame frame = new JFrame("Fyra i rad");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(winWidth, winWidth);
-		//frame.setResizable(false);
+		frame.setSize(winWidth, winHeight);
+		frame.setResizable(false);
 		frame.add(new JPanel());
 		frame.setLocationRelativeTo(null);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(board.getColor());
+		frame.add(panel);
 		frame.setVisible(true);
 
 	}
+
 	public static void main(String[] args) {
-	new Game(800, 600, 7, 6);
+		new Game(800, 600, 7, 6);
 	}
 
-//	private void print(Marker[][] board) {
-//		Graphics2D g2d = (Graphics2D) g;
-//		g2d.drawOval(0, 0, 80, 80);
-//		g2d.setColor(Color.white);
-//
-//	}
+	// private void drawCurrentBoard(Marker[][] board) {
+	// Graphics2D g2d = (Graphics2D) g;
+	// g2d.drawOval(0, 0, 80, 80);
+	// g2d.setColor(Color.white);
+	//
+	// }
 
 }
